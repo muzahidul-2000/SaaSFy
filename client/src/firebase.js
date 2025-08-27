e52b79c1547179a939198e83ff2dbc13c8f9cdbf
muzahidul-2000 <mdislam989100@gmail.com>
2025-08-27 12:09:49 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -13,5 +13,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); // ✅ now db works
-
-console.log("🔥 Firebase Config:", firebaseConfig);
